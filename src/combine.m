d12e94e82e8835d97e1ed40249ec6b39001806db
Michael Werger <mwerger@t-online.de>
2016-09-11 17:48:20 +0200

    added combine.m

---
+++ src/combine.m
@@ -0,0 +1,16 @@
+
+
+
+
+clear result
+
+result(1).object = object;
+result(1).rgb = corr(1).rgb;
+
+for i = [2:sz(1)]
+    
+    result(1).rgb = result(1).rgb + rgbshiftdxdy( corr(1).rgb, corr(i).rgb);
+end
+
+imagesc(result(1).rgb)
+
